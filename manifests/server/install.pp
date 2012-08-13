38149a3fb220f526297a5f7ab71ebf6f211d3e1c
angus <ags@sdx.com.au>
2012-08-13 17:49:48 +1000

    trying to custom pg_hba.conf

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -46,25 +46,7 @@ class postgresql::server::install(
     require => Package['postgresql'],
   }
 
-  file {"pg_hba.conf":
-    path    => "/etc/postgresql/${version}/main/pg_hba.conf",
-    content => template("postgresql/${version}/pg_hba.conf.erb"),
-    owner   => 'postgres',
-    group   => 'postgres',
-    mode    => 640,
-    replace => false,
-    require => [Package['postgresql'],User['postgres']],
-    notify  => Service['postgresql'],
-  }
 
-  file {"postgresql.conf":
-    path    => "/etc/postgresql/${version}/main/postgresql.conf",
-    content => template("postgresql/${version}/postgresql.conf.erb"),
-    owner   => 'postgres',
-    group   => 'postgres',
-    require => Package['postgresql'],
-    notify  => Service['postgresql'],
-  }
 
 }  
 
